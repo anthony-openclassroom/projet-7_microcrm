384b00022746aad5bfda77eba2ef19b492c7cd06
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-06-10 09:32:53 +0200

    fix(back): add https://localhost to CORS allowed origins

--- back/src/main/java/com/openclassroom/devops/orion/microcrm/SpringDataRestCustomization.java
+++ back/src/main/java/com/openclassroom/devops/orion/microcrm/SpringDataRestCustomization.java
@@ -12,7 +12,7 @@ public class SpringDataRestCustomization implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         config.exposeIdsFor(Person.class, Organization.class);
         cors.addMapping("/**")
-                .allowedOrigins("http://localhost", "https://microcrm.example.com")
+                .allowedOrigins("http://localhost", "https://localhost")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .exposedHeaders("Access-Control-Allow-Origin")
                 .allowCredentials(false).maxAge(3600);
